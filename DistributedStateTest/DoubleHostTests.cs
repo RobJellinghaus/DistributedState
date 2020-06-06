081ff6c4e91725769db03152f7e1067297c811c7
Rob Jellinghaus <rjellinghaus@live.com>
2020-06-05 22:55:28 -0700

    Yay! DIstributed delete worked first time!

--- DistributedStateTest/DoubleHostTests.cs
+++ DistributedStateTest/DoubleHostTests.cs
@@ -126,11 +126,8 @@ namespace Distributed.State.Test
             // wait until the proxy for the new object makes it to the other host
             WaitUtils.WaitUntil(new[] { host, host2 }, () =>
                 host2.NetPeers.Count() == 1
-                && host2.ProxiesForPeer(host2.NetPeers.First()).Count == 1);
-
-            // wait until the proxy for the new object makes it to the first host
-            WaitUtils.WaitUntil(new[] { host, host2 }, () =>
-                host.NetPeers.Count() == 1
+                && host2.ProxiesForPeer(host2.NetPeers.First()).Count == 1
+                && host.NetPeers.Count() == 1
                 && host.ProxiesForPeer(host.NetPeers.First()).Count == 1);
 
             DistributedObject host2Proxy = host2.ProxiesForPeer(host2.NetPeers.First()).Values.First();
@@ -181,11 +178,8 @@ namespace Distributed.State.Test
                 // wait until the proxy for the new object makes it to the other host
                 WaitUtils.WaitUntil(new[] { host, host2 }, () =>
                     host2.NetPeers.Count() == 1
-                    && host2.ProxiesForPeer(host2.NetPeers.First()).Count == 1);
-
-                // wait until the proxy for the new object makes it to the first host
-                WaitUtils.WaitUntil(new[] { host, host2 }, () =>
-                    host.NetPeers.Count() == 1
+                    && host2.ProxiesForPeer(host2.NetPeers.First()).Count == 1
+                    && host.NetPeers.Count() == 1
                     && host.ProxiesForPeer(host.NetPeers.First()).Count == 1);
 
                 DistributedObject host2Proxy = host2.ProxiesForPeer(host2.NetPeers.First()).Values.First();
@@ -200,5 +194,45 @@ namespace Distributed.State.Test
             // now after things settle down there should be no proxy
             WaitUtils.WaitUntil(new[] { host }, () => host.PeerCount == 0 && host.ProxyPeerCount == 0);
         }
+
+        [Test]
+        public void HostCreateAfterConnectionThenDelete()
+        {
+            var testWorkQueue = new TestWorkQueue();
+
+            // the first host under test
+            using DistributedHost host = new DistributedHost(
+                testWorkQueue,
+                DistributedHost.DefaultListenPort,
+                isListener: true,
+                disconnectTimeout: 10000000); // we want to be able to debug
+
+            // construct second host
+            using DistributedHost host2 = new DistributedHost(
+                testWorkQueue,
+                DistributedHost.DefaultListenPort,
+                isListener: false,
+                disconnectTimeout: 10000000);
+
+            // make sure the hosts know what to do with ThingMessages
+            host.RegisterWith(ThingMessages.Register);
+            host2.RegisterWith(ThingMessages.Register);
+
+            // host could start announcing also, but host2 isn't listening so it wouldn't be detectable
+            host2.Announce();
+
+            // create object
+            var distributedThing = new DistributedThing(host, new LocalThing());
+
+            // wait until the proxy for the new object makes it to the other host
+            WaitUtils.WaitUntil(new[] { host, host2 }, () => host2.NetPeers.Count() == 1 && host2.ProxiesForPeer(host2.NetPeers.First()).Count == 1);
+
+            // now delete the object
+            distributedThing.Delete();
+
+            // wait until the delete messages flow around
+            WaitUtils.WaitUntil(new[] { host, host2 }, () => host.ProxiesForPeer(host.NetPeers.First()).Count == 0);
+        }
+
     }
 }
