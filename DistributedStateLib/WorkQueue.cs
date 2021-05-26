3539de9418dea609d158885e5245b214e6954715
ROB-TUFLAPTOP <rjellinghaus@live.com>
2021-05-25 21:58:19 -0700

    oops

--- DistributedStateLib/WorkQueue.cs
+++ DistributedStateLib/WorkQueue.cs
@@ -2,7 +2,7 @@
 using System;
 using System.Collections.Generic;
 
-namespace Distributed.State.Test
+namespace Distributed.State
 {
     /// <summary>
     /// Work queue that runs work when polled.
